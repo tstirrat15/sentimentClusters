fb482e64672267298224acd6c19df82d05a5579d
tstirrat15 <tstirrat@gmail.com>
2015-01-23 15:54:23 -0600

    messed with file paths

---
+++ settings.py
@@ -0,0 +1,8 @@
+import os
+
+BASE_DIR = os.getcwd()
+DATA_DIR = os.path.join(BASE_DIR, "data")
+
+ID_DIR = os.path.join(DATA_DIR, "ids")
+SQL_DIR = os.path.join(DATA_DIR, "sqlite")
+JSON_DIR = os.path.join(DATA_DIR, "json")
