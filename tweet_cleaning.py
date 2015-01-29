e95f23b0c0ab2dbc7a9f16e61f12028b7285c5af
tstirrat15 <tstirrat@gmail.com>
2015-01-29 13:54:14 -0600

    Renamed jaccard_indices script to reflect what I'm actually doing with it

--- tweet_cleaning.py
+++ tweet_cleaning.py
@@ -2,17 +2,13 @@
 import os.path
 import re
 import dataset
-import itertools
 import argparse
 
 # NTLK imports
 import nltk.corpus
 
-# Distance import
-from distance import jaccard
-
-
 # Someday: set this up for parallel processing, based on number of cores
+
 
 class Tweet(object):
     """Holds attributes of tweets"""
@@ -91,34 +87,14 @@ if __name__ == "__main__":
     db = dataset.connect("sqlite:///" + db_path)
 
     # Make list of Tweet objects out of db rows
-    tweets = [Tweet(row["id_str"], row["text"]) for row in db["tweets"].all()]
+    tweets = (Tweet(row["id_str"], row["text"]) for row in db["tweets"].all())
 
     # Initialize the tweet processor
     p = TweetProcessor()
 
-    # Clean all tweets on one pass.
-    for tweet in tweets:
-        tweet.cleaned = p.process_tweet(tweet.content)
-
+    # Clean all tweets on one pass, and write cleaned tweets to the
+    # output file
     with open(network_path, "w") as output:
-        # Iterates over all pairs without repetition
-        for first, second in itertools.combinations(tweets, 2):
-
-            # Only try and calculate distance if both sets are nonempty
-            # Otherwise jaccard index has a divide by zero error
-            if first.cleaned and second.cleaned:
-                distance = 1 - jaccard(first.cleaned, second.cleaned)
-            else:
-                distance = 0
-
-            # Only write to output if distance is nonzero - makes the
-            # network file smaller and easier to handle. Can easily
-            # be parsed in later.
-
-            # Output format is based on GANXiS's input specification.
-            # GANXiS looks for edges in both directions, so print tweet
-            # IDs in both orders.
-            if distance:
-                output.write("{0} {1} {2}\n{1} {0} {2}\n".format(first.tweet_id,
-                                                                 second.tweet_id,
-                                                                 distance))
+        for tweet in tweets:
+            tweet.cleaned = p.process_tweet(tweet.content)
+            output.write("{0}\t{1}\n".format(tweet.tweet_id, " ".join(tweet.cleaned)))
